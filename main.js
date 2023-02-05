340f0e63a5cc27175994bb3b5e434593a530fd1c
angelopietro <angelopietror@gmail.com>
2023-02-05 19:36:57 -0300

    feat: lista de tarefas utilizando jQuery

--- main.js
+++ main.js
@@ -1,38 +1,21 @@
+$(document).ready(function () {
+  $('form').submit(function (event) {
+    event.preventDefault();
+  });
 
-const contato = [];
-function cadastrarContato() {
-  const tbody = document.getElementById ('lista_contatos');
-  const nomeContato = document.getElementById ('nome_contato').value;
-  const telefoneContato = document.getElementById('telefone_contato').value;
-  let linha = '';
-  const verificaSeContatoExiste = contato.some (res => res.nome === nomeContato);
+  $('#nomeTarefa').keydown(function (event) {
+    if (event.keyCode == 13) {
+      $('button').click();
+    }
+  });
 
-  if (nomeContato === '') { 
-    alert(`Você precisa digitar o nome do contato!`);
-    return;
-  }
-  
-  if (telefoneContato === '') { 
-    alert(`Você precisa digitar o número de telefone do contato!`);
-    return;
-  }
-  
-  if (verificaSeContatoExiste) {
-    alert(`Contato ${nomeContato} já existe na lista!`);
-    return;
-  }
+  $('button').click(function () {
+    const nomeTarefa = $('#nomeTarefa').val();
+    $('ul').prepend('<li>' + nomeTarefa + '</li>');
+    $('#nomeTarefa').val('');
+  });
 
-  contato.unshift ({nome: nomeContato, telefone: telefoneContato});
-
-  for (i = 0; i < contato.length; i++) {
-    linha += '<tr>';
-    linha += `<td>${contato[i].nome}</td>`;
-    linha += `<td>${contato[i].telefone}</td>`;
-    linha += `</tr>`;
-  }
-
-  document.getElementById('nome_contato').value = '';
-  document.getElementById('telefone_contato').value = '';
-
-  return (tbody.innerHTML = linha);
-}
+  $(document).on('click', 'li', function () {
+    $(this).toggleClass('tarefa-concluida');
+  });
+});
