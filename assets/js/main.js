a1eca5f7824e2875e328d1782cdfad93a9bbf35b
angelopietro <angelopietror@gmail.com>
2023-11-03 11:49:32 -0300

    Exercício ajax - módulo 23

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,29 @@
-$( document ).ready( function () {
-    $( "#tel" ).mask( "(00) 00000-0000", {
-        placeholder: "(00) 00000-00000",
-    } );
+document.addEventListener('DOMContentLoaded', () => {
+    try {
+        const username = 'angelopietro';
+        const url = `https://api.github.com/users/${username}`;
+        const profileAvatar = document.querySelector('#profileAvatar');
+        const profileName = document.querySelector('#profileName');
+        const profileUserName = document.querySelector('#profileUserName');
+        const totalPublicRepositories = document.querySelector('#totalPublicRepositories');
+        const totalFollowers = document.querySelector('#totalFollowers');
+        const totalFollowing = document.querySelector('#totalFollowing');
+        const profileUrl = document.querySelector('#profileUrl');
 
-    $( "form" ).validate( {
-        rules: {
-            nome: {
-                required: true,
-            },
-            email: {
-                required: true,
-                email: true,
-            },
-            mensagem: {
-                required: true,
-            },
-        },
-        messages: {
-            nome: "Por favor, digite seu nome completo",
-            email: "Por favor, digite seu email",
-            mensagem: "Por favor deixe uma mensagem",
-        },
-        submitHandler: function ( form ) {
-            console.log( form );
-            $( "#nome" ).val( "" );
-            $( "#email" ).val( "" );
-            $( "#tel" ).val( "" );
-            $( "#mensagem" ).val( "" );
-        },
-    } );
-} );
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                profileUserName.innerText = data.name;
+                profileAvatar.src = data.avatar_url;
+                profileName.innerText = data.name;
+                profileUserName.innerText = `@${data.login}`;
+                totalPublicRepositories.innerText = data.public_repos;
+                totalFollowers.innerText = data.followers;
+                totalFollowing.innerText = data.following;
+                profileUrl.href = data.html_url;
+            });
+    } catch (error) {
+        const displayError = document.querySelector('#displayError');
+        displayError.innerText = `${error.message}`;
+    }
+});
