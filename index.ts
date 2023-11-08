2843266655c3b9df08d80d72d6b6adf78751b6c0
angelopietro <angelopietror@gmail.com>
2023-11-08 13:55:08 -0300

    Exercício módulo 26

---
+++ index.ts
@@ -0,0 +1,18 @@
+function multiplicacao(valorA: number, valorB: number): number {
+    return valorA * valorB;    
+}
+
+const respostaMultiplicacao = multiplicacao(10, 12);
+
+console.log(respostaMultiplicacao);
+
+
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;   
+}
+
+const respostaSaudacao = saudacao('Ângelo Pietro');
+
+console.log(respostaSaudacao);
+
