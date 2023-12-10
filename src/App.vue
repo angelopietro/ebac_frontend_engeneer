326b8aaddd1db465d182ebc94d906e8ff14cc47d
angelopietro <angelopietror@gmail.com>
2023-12-10 15:24:34 -0300

    Calculadora VueJS

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import CalculadoraBasica from "./components/CalculadoraBasica.vue";
+
+export default {
+  name: "App",
+  components: {
+    CalculadoraBasica,
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <CalculadoraBasica />
+  </div>
+</template>
